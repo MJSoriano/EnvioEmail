f26525046905aec62527e91dd1e816ba0c365165
Soriano <soriano.mateus@outlook.com>
2018-11-01 00:06:08 -0300

    Finalizado
    Versão de entrega

---
+++ email-sender/src/main/java/EmailSender.java
@@ -0,0 +1,14 @@
+
+public class EmailSender {
+	public static void main(String[] args) {
+		final String fromEmail = "soriano.dev.2016@gmail.com";
+		final String password = "unitedfederationofplanets@24";
+		final String toEmail = "soriano.mateus@gmail.com";
+		
+		System.out.println("Initializing email send");
+		
+		EmailConfig config = new EmailConfig();
+		
+		config.sendEmail(fromEmail, password, toEmail, "Subject", "Email Body");
+}
+}
