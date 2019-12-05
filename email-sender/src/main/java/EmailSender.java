4c1660928b27d6f547f6f5f5607d76838706df7e
Mateus Soriano <soriano.mateus@outlook.com>
2019-12-05 11:50:21 -0300

    Update EmailSender.java

--- email-sender/src/main/java/EmailSender.java
+++ email-sender/src/main/java/EmailSender.java
@@ -2,7 +2,7 @@
 public class EmailSender {
 	public static void main(String[] args) {
 		final String fromEmail = "soriano.dev.2016@gmail.com";
-		final String password = "unitedfederationofplanets@24";
+		final String password = "uts@24";
 		final String toEmail = "soriano.mateus@gmail.com";
 		
 		System.out.println("Initializing email send");
